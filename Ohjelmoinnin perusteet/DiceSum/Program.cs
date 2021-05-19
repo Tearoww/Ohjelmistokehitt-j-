349c55055e730edcf54f21267389d720df211cd0
laeroy <52239281+Laeroy@users.noreply.github.com>
2021-05-19 15:50:16 +0300

    Added custom round amount functionality to DiceSum

--- Ohjelmoinnin perusteet/DiceSum/Program.cs
+++ Ohjelmoinnin perusteet/DiceSum/Program.cs
@@ -8,31 +8,51 @@ namespace DiceSum
         /// Rolls dice five times and adds the results together
         /// </summary>
         /// <returns>The sum of five dice as an integer</returns>
-        private static int RollDice()
+        private static int RollDice(int RoundAmount)
         {
             Random pool = new Random();
-            int[] results = new int[5];
             int sum = 0;
 
-            for (int i = 0; i < 5; i++)
-            {
-                results[i] = pool.Next(1, 7);
-            }
-
-            for (int i = 0; i < results.Length; i++)
-            {
-                sum += (int)results.GetValue(i);
-            }
-
+            for (int i = 1; i <= RoundAmount; i++)
+                sum += pool.Next(1, 7);
             return sum;
         }
+
         /// <summary>
         /// Prints the sum of two dice to the user
         /// </summary>
         private static void PrintSum()
         {
-            Console.WriteLine(RollDice() + RollDice());
+            int RoundAmount = AmountOfRounds();
+            Console.WriteLine(RollDice(RoundAmount) + RollDice(RoundAmount));
         }
+
+        /// <summary>
+        /// Sets the amount of rounds to roll dice
+        /// </summary>
+        /// <returns>The amount on rounds</returns>
+
+        private static int AmountOfRounds()
+        {
+            Console.Write("Montako kierrosta noppia heitetään?: ");
+            int RoundAmount = SelectionParser();
+            return RoundAmount;
+        }
+
+        /// <summary>
+        /// Parses user selection into integer
+        /// </summary>
+        /// <returns>A valid integer</returns>
+        private static int SelectionParser()
+        {
+            int selection;
+            bool intCheck;
+            do{
+               intCheck = int.TryParse(Console.ReadLine(), out selection);
+            }while(!intCheck);
+            return selection;
+        }
+
         static void Main(string[] args)
         {
             PrintSum();
