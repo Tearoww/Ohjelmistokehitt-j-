35cf594e207afa23a31877bd2da2e1bb7789d65f
Tearoww <74559498+Tearoww@users.noreply.github.com>
2021-05-24 09:33:22 +0300

    Initial commit of version three

--- Ohjelmoinnin perusteet/DiceSum/Program.cs
+++ Ohjelmoinnin perusteet/DiceSum/Program.cs
@@ -5,16 +5,16 @@ namespace DiceSum
     class Program
     {
         /// <summary>
-        /// Rolls dice five times and adds the results together
+        /// Rolls dice three times and adds the results together
         /// </summary>
-        /// <returns>The sum of five dice as an integer</returns>
+        /// <returns>The sum of dice throws as an integer</returns>
         private static int RollDice()
         {
             Random pool = new Random();
-            int[] results = new int[5];
+            int[] results = new int[3];
             int sum = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 results[i] = pool.Next(1, 7);
             }
@@ -27,7 +27,7 @@ namespace DiceSum
             return sum;
         }
         /// <summary>
-        /// Prints the sum of two dice to the user
+        /// Prints the sum of dice throws to the user
         /// </summary>
         private static void PrintSum()
         {
